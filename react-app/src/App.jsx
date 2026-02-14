6d9e7ad287bb26ade378531e671d4892b4b19adb
Cvirti <vl176932@gmail.com>
2026-02-14 17:32:37 +0700

    add

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,29 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Header from '../src/layout/header/header'
-import MainForm from '../src/layout/form/main-form'
-import MapKit from './layout/mapKit/MapKit'
-import InfiniteSlider from './layout/infiniteSlider/InfiniteSlider'
-import Why from './layout/why/Why'
-import Service from './layout/servic/Service'
-import TimeBlock from './layout/timeBlock/TimeBlock'
-import Data from './layout/сardSlider/Data'
-import Footer from './layout/footer/Footer'
-import SectionGlobal from './layout/sectionGlobal/SectionGlobal'
-import ContactForm from './layout/contactForm/ContactForm'
+import Header from '../src/layout/header/header';
+import MainForm from '../src/layout/form/main-form';
+import MapKit from './layout/mapKit/MapKit';
+import InfiniteSlider from './layout/infiniteSlider/InfiniteSlider';
+import Why from './layout/why/Why';
+import Service from './layout/servic/Service';
+import TimeBlock from './layout/timeBlock/TimeBlock';
+import Data from './layout/сardSlider/Data';
+import Footer from './layout/footer/Footer';
+import SectionGlobal from './layout/sectionGlobal/SectionGlobal';
+import ContactForm from './layout/contactForm/ContactForm';
 
 function App() {
+  const [showButton, setShowButton] = useState(false);
+
+  // Jivo Widget
   useEffect(() => {
-    var widget_id = 'NhrSDE7n5m';
-    var d = document;
-    var w = window;
-    
+    const widget_id = 'NhrSDE7n5m';
+    const d = document;
+    const w = window;
+
     function loadJivo() {
-      var s = document.createElement('script');
+      const s = document.createElement('script');
       s.type = 'text/javascript';
       s.async = true;
       s.src = '//code.jivo.ru/widget/' + widget_id;
-      var ss = document.getElementsByTagName('script')[0];
+      const ss = document.getElementsByTagName('script')[0];
       ss.parentNode.insertBefore(s, ss);
     }
 
@@ -37,23 +40,41 @@ function App() {
       }
     }
 
-    // Cleanup function (опционально)
     return () => {
-      // Удаляем скрипт при размонтировании компонента
       const jivoScript = document.querySelector('script[src*="jivo"]');
-      if (jivoScript) {
-        jivoScript.remove();
-      }
-      // Также удаляем глобальный объект Jivo
+      if (jivoScript) jivoScript.remove();
       delete window.jivo_onload;
       delete window.jivo_init;
     };
   }, []);
 
-  return (
-    <div className="App">
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
 
-      <Header />
+    window.addEventListener('scroll', handleScroll);
+    
+    handleScroll();
+    
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+      <div className="App">     
+      <Header 
+      />
       <MainForm />
       <InfiniteSlider />
       <Why />
@@ -64,6 +85,12 @@ function App() {
       <ContactForm />
       <SectionGlobal />
       <Footer />
+      <button
+        className={`to-top-btn ${showButton ? 'show' : ''}`}
+        onClick={scrollToTop}
+      >
+        ↑
+      </button>
     </div>
   );
 }
